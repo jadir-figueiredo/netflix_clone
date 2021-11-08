67cdae7db4f874e46ac6c90626ae1d57f8e20683
jadirf31 <jadirf31@gmail.com>
2021-11-08 06:48:55 +0000

    atualizações

--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -8,7 +8,17 @@ export default ({item}) => {
             backgroundPosition: 'center',
             backgroundImage: `url(https://image.tmdb.org/t/p/original${item.backdrop_path})`
         }}>
-            <div>{item.original_name}</div>
+            <div className="featured--vertical">
+                <div className="featured--horizontal">
+                    <div className="featured--name">{item.original_name}</div>
+                    <div className="featured--info">
+                        <div className="featured--points">{item.vote_average} pontos</div>
+                        <div className="featured--year">2099</div>
+                        <div className="featured--seasons">{item.number_of_seasons} Temporada{item.number_of_seasons !== 1 ? 's' : ''}</div>
+                    </div>
+                    <div className="featured--desccription">{item.overview}</div>
+                </div>
+            </div>
         </section>
     )
 }
