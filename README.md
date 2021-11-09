1f5896c1068dd0783e6d952622d99c7edef09acc
jadirf31 <jadirf31@gmail.com>
2021-11-09 22:05:37 +0000

    atualizações

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # netflix_clone
 Projeto de clonagem de uma page da Netflix usando ReactJs
+
+##
+Foi usada uma API (TMDB) para utilizar as informações da Netflix.
+
--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -2,6 +2,13 @@ import React from 'react';
 import './FeaturedMovie.css';
 
 export default ({item}) => {
+
+    let firstDate = new Date(item.first_air_date);
+    let genres = [];
+    for(let i in item.genres) {
+        genres.push( item.genres[i].name );
+    }
+
     return (
         <section className="featured" style={{
             backgroundSize: 'cover',
@@ -13,13 +20,16 @@ export default ({item}) => {
                     <div className="featured--name">{item.original_name}</div>
                     <div className="featured--info">
                         <div className="featured--points">{item.vote_average} pontos</div>
-                        <div className="featured--year">2099</div>
+                        <div className="featured--year">{firstDate.getFullYear()}</div>
                         <div className="featured--seasons">{item.number_of_seasons} Temporada{item.number_of_seasons !== 1 ? 's' : ''}</div>
                     </div>
-                    <div className="featured--desccription">{item.overview}</div>
-                    <div className="featured--buttons"></div>
+                    <div className="featured--description">{item.overview}</div>
+                    <div className="featured--buttons">
+                        <a href="{`/watch/${item.id}`}" className="featured--watchbutton">► Assistir</a>
+                        <a href="{`/list/add/${item.id}`}" className="featured--mylistbutton">+ Minha Lista</a>
+                    </div>
 
-                    <div className="featured--genres"><strong>Gêneros</strong> ...</div>
+                    <div className="featured--genres"><strong>Gêneros</strong> {genres.join(', ')}</div>
                 </div>
             </div>
         </section>
