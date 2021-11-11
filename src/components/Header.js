261c4de330060c94c8b15c0bad4d625ff7d28e1f
jadirf31 <jadirf31@gmail.com>
2021-11-11 08:37:36 +0000

    atualizações

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,11 @@ import './Header.css';
 export default () => {
     return (
         <header>
-            ...
+            <div className="header--logo">
+                <a href="/">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1024px-Netflix_2015_logo.svg.png" alt="Netflix"></img>
+                </a>
+            </div>
         </header>
     );
 }
