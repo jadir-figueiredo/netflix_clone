8f9d103e0f6dd33c29cd0793dcbe69e57c6c92a0
jadirf31 <jadirf31@gmail.com>
2021-11-11 08:59:56 +0000

    atualizações

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,8 +6,12 @@ export default () => {
         <header>
             <div className="header--logo">
                 <a href="/">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1024px-Netflix_2015_logo.svg.png" alt="Netflix"></img>
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1024px-Netflix_2015_logo.svg.png" alt="Netflix"></img>
                 </a>
+            </div>
+            <div className="header--user">
+                <a href="/"></a>
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" alt="Usuario" />
             </div>
         </header>
     );
